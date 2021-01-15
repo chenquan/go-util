77c3ceb8ca46c9ec25a70268bb2405e4546e0d0e
Chen Quan <chenquan@osai.club>
2021-01-15 19:32:00 +0800

    Fix 自动测试bug

--- task/job_test.go
+++ task/job_test.go
@@ -1,18 +1,25 @@
-/*
- *    Copyright 2021 Chen Quan
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- *
- */
-
 package task
+
+import (
+	"log"
+	"testing"
+	"time"
+)
+
+func TestJob(t *testing.T) {
+	var jobFunc JobFunc = func() error {
+		time.Sleep(time.Second * 5)
+		return nil
+	}
+	job := NewJob(jobFunc)
+	log.Println("1.运行")
+	_ = job.Run()
+	time.Sleep(time.Second)
+	log.Println("2.停止")
+	job.Stop()
+	time.Sleep(time.Second)
+	log.Println("2.运行")
+	_ = job.RunWithDeadline(time.Now().Add(time.Millisecond * 2))
+	_ = job.RunWithTimeout(time.Millisecond * 2)
+
+}
--- task/task_test.go
+++ task/task_test.go
@@ -1,18 +1,48 @@
-/*
- *    Copyright 2021 Chen Quan
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- *
- */
-
 package task
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestTask(t *testing.T) {
+	task := NewTask()
+	var JobFunc1 JobFunc = func() error {
+		fmt.Println("第一个任务，我运行了")
+		return nil
+	}
+	var JobFunc2 JobFunc = func() error {
+		fmt.Println("第二个任务，我运行了")
+		return nil
+	}
+	newJob1 := NewJob(JobFunc1)
+	newJob2 := NewJob(JobFunc2)
+	jobId1 := task.Add(newJob1)
+	jobId2 := task.Add(newJob2)
+	fmt.Println(jobId1)
+	if jobId1 == "" {
+		t.Errorf("Add error")
+
+	}
+	job1 := task.Get(jobId1)
+	job2 := task.Get(jobId2)
+	if job1 != newJob1 {
+		t.Errorf("Get error")
+	}
+	err := task.Run(jobId1)
+	err = job2.Run()
+	if err != nil {
+		fmt.Println(err)
+	}
+	time.Sleep(time.Millisecond * 5)
+	task.Stop(jobId1)
+	job2.Stop()
+	time.Sleep(time.Millisecond * 5)
+
+	id := job1.JobId()
+	task.Delete(id)
+	if task.Get(id) != nil {
+		t.Errorf("Delete error")
+	}
+}
